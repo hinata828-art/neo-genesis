e2aed4b3831304d2c1ac0bd5d9f18897488ba532
toyama-ko <2501223s.asojuku@gmail.com>
2025-11-07 15:46:53 +0900

    ヘッダー調整

--- common/header.php
+++ common/header.php
@@ -1,36 +1,43 @@
 <?php
+// このファイルが require される前に、
+// session_start() は実行済みであると想定します。
 ?>
 <header>
     <!-- 上段：ロゴ、カート、会員情報 -->
     <div class="top-row">
-        <div 
-            class="hamburger-menu navbar-burger" 
-            :class="{'is-active': isActive}" 
-            @click="toggleButton"
-        >
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
         
-        <!-- ロゴ (ホームへのリンク) -->
-        <div class="header-logo">
-            <a href="G-8_home.php">
-                <img src="../img/NishimuraOnline.png" alt="ロゴ" class="logo-image">
-            </a>
+        <!-- ★修正：左側（ハンバーガーとロゴ）をグループ化 -->
+        <div class="header-left">
+            <!-- ハンバーガーメニュー (Bulmaのnavbar-burgerを流用) -->
+            <div 
+                class="hamburger-menu navbar-burger" 
+                :class="{'is-active': isActive}" 
+                @click="toggleButton"
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
+            <!-- ロゴ (ホームへのリンク) -->
+            <div class="header-logo">
+                <a href="G-8_home.php">
+                    <img src="../img/NishimuraOnline.png" alt="ロゴ" class="logo-image">
+                </a>
+            </div>
         </div>
 
-        <!-- ボタン類 (カート・会員情報) -->
-        <div class="header-icons">
+        <!-- ★修正：右側（アイコン群）のクラス名を変更 -->
+        <div class="header-right-icons">
             <!-- 会員情報 (アイコン) -->
             <div class="header-user">
-                <a href="../フロント/G-4_member-information.php">
+                <a href="G-4_member-information.php">
                     <img src="../img/icon.png" alt="会員情報" class="user-icon-image">
                 </a>
             </div>
             <!-- カート -->
             <div class="header-cart">
-                <a href="../フロント/G-10_cart.php">
+                <a href="G-10_cart.php">
                     <img src="../img/cart.png" alt="カート">
                     <label>カート</label>
                 </a>
@@ -39,7 +46,7 @@
     </div>
 
     <!-- 下段：検索フォーム -->
-    <form action="../フロント/G-9_search-result.php" method="GET" class="bottom-row">
+    <form action="G-9_search-result.php" method="GET" class="bottom-row">
         
         <div class="search-container">
             <input type="text" name="keyword" placeholder="何をお探しですか？">
