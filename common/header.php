f7ba568066d89766fce93423e2c94a53ca8b7e14
hinata828-art <hinatasan828@gmail.com>
2025-11-07 11:48:34 +0900

    Create header.php

---
+++ common/header.php
@@ -0,0 +1,41 @@
+<!-- /common/header.php -->
+<header>
+    
+    <div class="top">
+        <div class="menu">
+            <a href="#"><img src="../img/menu.png" alt="メニューアイコン" width="100" height="50"></a>
+        </div>
+
+        <div class="header-button">
+            <div class="header-icon">
+                <a href="G-1_customer-form.php"><img src="../img/icon.png" alt="アカウントアイコン" width="50" height="50"></a>
+            </div>
+        </div>
+
+        <div class="header-cart">
+            <a href="#">
+                <img src="../img/cart.png" alt="カートアイコン">
+                <label>カート</label>
+            </a>
+        </div>
+    </div>
+
+    <div class="bottom">
+        <div class="select-box">
+            <select>
+                <option value="">すべて</option>
+                <option value="tv">テレビ</option>
+                <option value="refrigerator">冷蔵庫</option>
+                <option value="microwave">電子レンジ</option>
+                <option value="camera">カメラ</option>
+                <option value="headphone">ヘッドホン</option>
+                <option value="washer">洗濯機</option>
+            </select>
+        </div>
+
+        <div class="search-box">
+            <input type="text" placeholder="商品名で検索">
+            <button type="submit">🔍</button>
+        </div>
+    </div>
+</header>
