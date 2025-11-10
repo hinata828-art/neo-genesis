ac4166515a13f9c03870bcc85d99591fe625ee24
oonokouki <2401029@s.asojuku.ac.jp>
2025-11-10 10:56:52 +0900

    G-13 update

--- フロント/G-13_order-complete.php
+++ フロント/G-13_order-complete.php
@@ -50,7 +50,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>購入完了</title>
-    <link rel="stylesheet" href="G-13_order-complete.css">
+    <link rel="stylesheet" href="../css/G-13_order-complete.css">
 </head>
 <body>
 
