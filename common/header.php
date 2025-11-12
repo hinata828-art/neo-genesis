f67ab6cd7e7c9921de3d66dba4421abc718da962
hinata828-art <hinatasan828@gmail.com>
2025-11-12 10:51:19 +0900

    Update header.php

--- common/header.php
+++ common/header.php
@@ -1,8 +1,6 @@
 <header>
-    <!-- 上段：ロゴ、カート、会員情報 -->
     <div class="top-row">
         
-        <!-- 左側：ハンバーガー＋ロゴ -->
         <div class="header-left">
             <div class="hamburger-menu navbar-burger" @click="toggleButton">
                 <span></span>
@@ -17,26 +15,26 @@
             </div>
         </div>
 
-<!-- 右側：ユーザー・カート -->
-        <div class="header-user">
-            <a href="../フロント/G-4_member-information.php">
-                <img src="../img/icon.png" alt="会員情報" class="user-icon"> 
-            </a>
+        <div class="header-right-icons">
+            
+            <div class="header-user">
+                <a href="../フロント/G-4_member-information.php">
+                    <img src="../img/icon.png" alt="会員情報" class="user-icon"> 
+                </a>
+            </div>
 
-                    <div class="header-cart">
-            <a href="../フロント/G-11_cart.php">
-                <img src="../img/cart.png" alt="カートアイコン">
-                <label>カート</label>
-            </a>
+            <div class="header-cart">
+                <a href="../フロント/G-11_cart.php">
+                    <img src="../img/cart.png" alt="カートアイコン">
+                    <label>カート</label>
+                </a>
             </div>
         </div>
     </div>
 
-    <!-- 下段：カテゴリプルダウン＋検索フォーム -->
     <form action="../フロント/G-9_search-result.php" method="GET" class="bottom-row">
         <div class="search-container">
             
-            <!-- カテゴリプルダウン -->
             <select name="category" class="category-select">
                 <option value="">家電</option>
                 <option value="C01">テレビ</option>
@@ -49,10 +47,8 @@
                 <option value="C08">スマートフォン</option>
             </select>
 
-            <!-- 検索入力 -->
             <input type="text" name="keyword" placeholder="何をお探しですか？">
 
-            <!-- 検索ボタン -->
             <button type="submit" class="search-button">
                 <img src="../img/kensaku.png" alt="検索">
             </button>
