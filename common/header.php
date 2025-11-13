182d1848caa132ab040c7c910db4556728db2d80
hinata828-art <hinatasan828@gmail.com>
2025-11-13 11:22:16 +0900

    Update header.php

--- common/header.php
+++ common/header.php
@@ -54,4 +54,33 @@
             </button>
         </div>
     </form>
+    <div class="top-row">
+        </div>
+
+    <form action="../フロント/G-10_product-list.php" method="GET" class="bottom-row">
+        </form>
+    
+    <?php
+    // パンくずリストの配列を受け取る（存在しない場合は空配列）
+    $breadcrumbs = $breadcrumbs ?? [];
+
+    // ホームは必ず先頭に追加
+    array_unshift($breadcrumbs, ['name' => 'ホーム', 'url' => '../フロント/G-8_home.php']);
+    ?>
+
+    <nav class="breadcrumb">
+        <ul>
+            <?php foreach ($breadcrumbs as $index => $crumb): ?>
+                <li>
+                    <?php if (!empty($crumb['url']) && $index !== array_key_last($breadcrumbs)): ?>
+                        <a href="<?= htmlspecialchars($crumb['url'], ENT_QUOTES, 'UTF-8') ?>">
+                            <?= htmlspecialchars($crumb['name'], ENT_QUOTES, 'UTF-8') ?>
+                        </a>
+                    <?php else: ?>
+                        <?= htmlspecialchars($crumb['name'], ENT_QUOTES, 'UTF-8') ?>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
 </header>
