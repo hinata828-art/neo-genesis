a33bf30a9fe718c7fcba4293ca376a2e50acac1e
toyama-ko <2501223s.asojuku@gmail.com>
2025-11-17 10:54:21 +0900

    Update G-7_withdrawal-complete.php

--- フロント/G-7_withdrawal-complete.php
+++ フロント/G-7_withdrawal-complete.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/G-7_withdrawal-complete.css">
     <link rel="stylesheet" href="../css/header.css">
     <title>退会完了</title>
