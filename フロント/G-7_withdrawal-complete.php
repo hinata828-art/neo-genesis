eab2085613809ce67800a53bb4140aea69dae087
toyama-ko <2501223s.asojuku@gmail.com>
2025-11-17 10:35:07 +0900

    a

--- フロント/G-7_withdrawal-complete.php
+++ フロント/G-7_withdrawal-complete.php
@@ -2,17 +2,17 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/G-7退会完了.css">
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="../css/G-7_withdrawal-complete.css">
+    <link rel="stylesheet" href="../css/header.css">
     <title>退会完了</title>
 </head>
 <body>
     <?php require __DIR__ . '/../common/header.php'; ?>
-    <img src="img/NishimuraOnline.png" alt="ニシムラOnlineロゴ" class="logo">
+    <img src="../img/NishimuraOnline.png" alt="ニシムラOnlineロゴ" class="logo">
     <div class="message">
         退会が完了しました！<br>
         ご利用ありがとうございました！
     </div>
-    <a href="G-1ログインフォーム.php" class="btn">ホーム画面へ</a>
+    <a href="G-1_login-form.php" class="btn">ホーム画面へ</a>
 </body>
 </html>
