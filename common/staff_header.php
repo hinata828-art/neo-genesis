12695bfeda8e6e73f8a9edd1812bd491f31838ac
nana0mi8 <cooa007310@gmail.com>
2025-11-18 10:03:30 +0900

    ハンバーガメニュー

--- common/staff_header.php
+++ common/staff_header.php
@@ -13,4 +13,31 @@
   <div class="user-icon">
     <img src="../img/icon.png" alt="ユーザーアイコン">
   </div>
+
+  <!-- スライドメニュー -->
+<nav id="sideMenu" class="side-menu">
+    <ul>
+        <li><a href="../バック/.php">顧客管理</a></li>
+        <li><a href="../バック/G-22_product.php">商品管理</a></li>
+    </ul>
+</nav>
+
+<!-- メニューを閉じるための黒背景 -->
+<div id="overlay" class="menu-overlay"></div>
+
+<script>
+    const menu = document.getElementById("sideMenu");
+    const overlay = document.getElementById("overlay");
+    const burger = document.querySelector(".hamburger-menu");
+
+    burger.addEventListener("click", function () {
+        menu.classList.toggle("open");
+        overlay.classList.toggle("show");
+    });
+
+    overlay.addEventListener("click", function () {
+        menu.classList.remove("open");
+        overlay.classList.remove("show");
+    });
+</script>
 </header>
