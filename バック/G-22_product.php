2de274d274865c4f073176f4c14103ff59a2a022
hinata828-art <hinatasan828@gmail.com>
2025-11-21 11:43:42 +0900

    Update G-22_product.php

--- バック/G-22_product.php
+++ バック/G-22_product.php
@@ -117,7 +117,7 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
                   </table>
                 </div>
                 <div class="product-actions">
-                  <button onclick="location.href='G-23_product-detail.php?id=<?= $p['product_id'] ?>'">編集</button>
+                  <button onclick="location.href='G-23_product-detail.php?id=<?= $p['product_id'] ?>'">商品編集</button>
                   <button onclick="if(confirm('削除しますか？')) location.href='delete_product.php?id=<?= $p['product_id'] ?>'">削除</button>
                 </div>
               </div>
